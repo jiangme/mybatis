44c991f0da4378fa0098bd1db5796a5068e04d16
jiangme <2587556966@qq.comgit config --global user.email 2587556966@qq.com>
2019-04-22 22:19:21 +0800

    新增：mybatis-plus代码生成器

---
+++ mybatis-core/mybatis-generator/src/main/java/com/jiang/MybatisGeneratorApplication.java
@@ -0,0 +1,16 @@
+package com.jiang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MybatisGeneratorApplication {
+
+    public static void main(String[] args) {
+
+        System.out.println(System.getProperty("user.dir"));
+
+       // SpringApplication.run(MybatisGeneratorApplication.class, args);
+    }
+
+}
